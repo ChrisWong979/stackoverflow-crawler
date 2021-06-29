f6910339e89a59decea14fb7bc3b3f1af2d5066f
Chris Wong <wc38.wong@gmail.com>
2021-06-29 11:41:39 +0800

    Crawl questions with codesandbox

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"strings"
 	"time"
 
 	"github.com/chromedp/cdproto/cdp"
+	"github.com/chromedp/cdproto/dom"
 	"github.com/chromedp/chromedp"
 )
 
@@ -18,11 +18,10 @@ const (
 
 func main() {
 	var nodes []*cdp.Node
-	var buf []byte
 
 	ctx := context.Background()
 	options := []chromedp.ExecAllocatorOption{
-		chromedp.Flag("headless", false),
+		chromedp.Flag("headless", true),
 		chromedp.Flag("hide-scrollbars", false),
 		chromedp.Flag("mute-audio", false),
 		chromedp.Flag("auto-open-devtools-for-tabs", false),
@@ -39,25 +38,65 @@ func main() {
 
 	err := chromedp.Run(ctx,
 		chromedp.Navigate(domain + "/questions/tagged/javascript%20reactjs?sort=Newest&filters=NoAcceptedAnswer&edited=true"),
-		chromedp.Sleep(6 * time.Second),
+		chromedp.Sleep(3 * time.Second),
 		chromedp.Nodes(".question-hyperlink", &nodes, chromedp.NodeReady),
 		chromedp.ActionFunc(func(ctx context.Context) error {
 			for _, node := range nodes {
 				url, _ := node.Attribute("href")
 				if strings.HasPrefix(url, "https") == false {
-					fmt.Println(domain + url)
+					const selector = ".question p > a"
+					var res []byte
+					web := domain + url
+					err := chromedp.Run(ctx,
+						chromedp.Navigate(web),
+						chromedp.Evaluate(fmt.Sprintf(`document.querySelectorAll("%s")`, selector), &res),
+						chromedp.ActionFunc(func(ctx context.Context) error {
+							// to avoid blocking behavior
+							r := string(res)
+							if r != "" && r != "{}" {
+								var ids []cdp.NodeID
+								err := chromedp.Run(ctx,
+									chromedp.NodeIDs(selector, &ids),
+									chromedp.ActionFunc(func(ctx context.Context) error {
+										for _, id := range ids {
+											attributes, err := dom.GetAttributes(id).Do(ctx)
+											
+											for i, attribute := range attributes {
+												if attribute == "href" {
+													a := attributes[i + 1]
+													if strings.HasPrefix(a, "https://codesandbox.io") {
+														fmt.Println(a)
+														fmt.Println(web)
+													}
+												}
+
+											}
+											if err != nil {
+												return err
+											}
+										}
+										return nil
+									}),
+								)
+
+								if err != nil {
+									return err
+								}
+							}
+
+							return nil
+						}),
+					)	
+					if err != nil {
+						return err
+					}
 				}
 			}
-			fmt.Println(len(nodes))
 			return nil
 		}),
 		chromedp.Sleep(1 * time.Second),
-		chromedp.CaptureScreenshot(&buf),
 	)
 	if err != nil {
-		log.Fatal(err)
-	}
-	if err := ioutil.WriteFile("stackoverflow.png", buf, 0644); err != nil {
 		log.Fatal(err)
 	}
 }
